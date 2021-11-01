9c44f736c9360a6058af17f41c4f97d1a9ffe82a
Avinash Singh <avinash.singh@eka1.com>
2021-11-01 17:02:19 +0530

    Lexographicallly

--- Assignment/Recursion Assignment/RecursionPractice/src/PowerSet.java
+++ Assignment/Recursion Assignment/RecursionPractice/src/PowerSet.java
@@ -2,8 +2,8 @@ class PowerSet {
 
     public static void main(String[] args) {
         String str = "abc";
-        // power(str);
-        permuteRec(str,3,-1,"");
+        power(str);
+        // permuteRec(str, 3, -1, "");
     }
 
     static void power(String str) {
@@ -13,34 +13,33 @@ class PowerSet {
 
     static void powerHelper(String unpr, String pr) {
         if (unpr.length() == 0) {
-            if (pr.length() != 0)
-                System.out.print(pr + " ");
+            // if (pr.length() != 0)
+            // System.out.print(pr + " ");
             return;
         }
         char c = unpr.charAt(0);
+        System.out.print(pr + c + " ");
         powerHelper(unpr.substring(1), pr + c);
+        // System.out.print(pr + " ");
         powerHelper(unpr.substring(1), pr);
 
     }
 
-    static void permuteRec(String str, int n,
-                           int index, String curr)
-    {
+    static void permuteRec(String str, int n, int index, String curr) {
         // base case
         if (index == n) {
             return;
         }
         System.out.println(curr);
         for (int i = index + 1; i < n; i++) {
- 
+
             curr += str.charAt(i);
             permuteRec(str, n, i, curr);
- 
+
             // backtracking
             curr = curr.substring(0, curr.length() - 1);
         }
         return;
     }
- 
 
 }
